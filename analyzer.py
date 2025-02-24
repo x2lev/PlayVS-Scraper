d52ec6c826acfcc66276764453813ef8de0210c4
x2lev <lev.kryvenko@gmail.com>
2025-02-24 02:25:54 -0700

    added scraping of older seasons

--- analyzer.py
+++ analyzer.py
@@ -3,21 +3,25 @@ import json
 with open('data.json', 'r', encoding='UTF-8') as f:
     data = json.loads(f.read())
 
-print(data.pop(0))
+lines = []
+lines.append(data.pop(0))
 players = {}
 for match in data:
     for pos, series in enumerate(match):
         name = series['name']
         if name not in players:
-            players[name] = {'pos': [0, 0, 0, 0, 0], 'stage': {}, 'char': {}, 'opp': {}}
+            players[name] = {'season': [], 'pos': [0, 0, 0, 0, 0], 'stage': {}, 'char': {}, 'opp': {}}
         if pos not in players[name]['pos']:
             players[name]['pos'][pos] = 0
         players[name]['pos'][pos] += 1
         for game in series['games']:
+            season = game['season']
             stage = game['stage']
             char = game['char']
             opp = game['opp']
             result = game['result']
+            if season not in players[name]['season']:
+                players[name]['season'].append(season)
             if stage not in players[name]['stage']:
                 players[name]['stage'][stage] = {'win': 0, 'loss': 0}
             if char not in players[name]['char']:
@@ -29,27 +33,29 @@ for match in data:
             players[name]['char'][char][result] += 1
             players[name]['opp'][opp][result] += 1
 
-lines = []
-for name, player in players.items():
-    lines.append(f'\n{name}')
-    lines.append('  Position')
-    for i in range(5):
-        lines.append(f'    {i+1}: {player['pos'][i]} series')
-    lines.append('  Stages')
-    for stage, results in player['stage'].items():
-        w = results['win']
-        t = results['win'] + results['loss']
-        lines.append(f'    {stage}: {t} game(s), {round(w/t, 4)*100}% win rate')
-    lines.append('  Characters')
-    for char, results in player['char'].items():
-        w = results['win']
-        t = results['win'] + results['loss']
-        lines.append(f'    {char}: {t} game(s), {round(w/t, 4)*100}% win rate')
-    lines.append('  Opponents')
-    for opp, results in player['opp'].items():
-        w = results['win']
-        t = results['win'] + results['loss']
-        lines.append(f'    {opp}: {t} game(s), {round(w/t, 4)*100}% win rate')
+for name, player in sorted(players.items(), key=lambda t: list(t)[0]):
+    print(player['season'])
+    if any(s in ['Spring 2025', 'Fall 2024'] for s in player['season']) or name == 'Anpetu Chrisjohn':
+        lines.append(f'\n{name}')
+        lines.append(f'  Played in {', '.join(player['season'])}')
+        lines.append('  Position')
+        for i in range(5):
+            lines.append(f'    {i+1}: {player['pos'][i]} series')
+        lines.append('  Stages')
+        for stage, results in sorted(player['stage'].items(), key=lambda t: list(t)[1]['win'] + list(t)[1]['loss'], reverse=True):
+            w = results['win']
+            t = results['win'] + results['loss']
+            lines.append(f'    {stage}: {t} game(s), {round(w/t, 4)*100}% win rate')
+        lines.append('  Characters')
+        for char, results in sorted(player['char'].items(), key=lambda t: list(t)[1]['win'] + list(t)[1]['loss'], reverse=True):
+            w = results['win']
+            t = results['win'] + results['loss']
+            lines.append(f'    {char}: {t} game(s), {round(w/t, 4)*100}% win rate')
+        lines.append('  Opponents')
+        for opp, results in sorted(player['opp'].items(), key=lambda t: list(t)[1]['win'] + list(t)[1]['loss'], reverse=True):
+            w = results['win']
+            t = results['win'] + results['loss']
+            lines.append(f'    {opp}: {t} game(s), {round(w/t, 4)*100}% win rate')
 
 print('\n'.join(lines))
 
--- scraper.py
+++ scraper.py
@@ -6,10 +6,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 
-TEAM_URL = ''#'https://app.playvs.com/app/team/76862a5c-caec-48a8-ae2d-7793d54bf037'
+TEAM_URL = ''#'https://app.playvs.com/app/team/d52bebba-87a6-439f-b299-7ab951a01968'
 STANDINGS_URL = 'https://app.playvs.com/app/standings'
 
-TEAM_NAME = 'Kadet Smash Bros'
+TEAM_NAME = 'DSISD A'
 if not TEAM_NAME:
     TEAM_NAME = input('Enter the team name: ')
 
@@ -34,85 +34,123 @@ if not TEAM_URL:
     driver.find_element(By.XPATH, '//p[contains(text(),"League of Legends")]').click()
     driver.find_element(By.XPATH, '//p[contains(text(),"Super Smash Bros")]').click()
     driver.find_element(By.XPATH, '//p[contains(text(), "Spring") or contains(text(), "Fall")]').click()
-    driver.find_element(By.CSS_SELECTOR, 'li[data-cy="Spring 2025"]').click()
+    driver.find_element(By.XPATH, '//p[text()="Spring 2025"]').click()
 
     driver.implicitly_wait(5)
 
     # Find the team
     action_chains.send_keys(Keys.PAGE_DOWN).send_keys(Keys.PAGE_DOWN).perform()
-    team = []
     while True:
-        teams = driver.find_element(By.XPATH, '//p[text()="Overall"]/..//following-sibling::div/*')
-        try:
-            team = teams.find_element(By.XPATH, f'//p[text()="{TEAM_NAME}"]')
+        time.sleep(0.5)
+        overall = driver.find_element(By.XPATH,
+                                      '//p[text()="Overall"]/..//following-sibling::div/*')
+        team_p = None
+        for p in overall.find_elements(By.CSS_SELECTOR, 'p'):
+            if p.text == TEAM_NAME:
+                team_p = p
+                break
+        else:
+            overall.send_keys(Keys.PAGE_DOWN)
+        
+        if team_p:
             try:
-                time.sleep(0.5)
-                team.click()
+                team_p.click()
                 break
             except ElementClickInterceptedException:
-                teams.send_keys(Keys.ARROW_DOWN)
-        except NoSuchElementException:
-            teams.send_keys(Keys.PAGE_DOWN)
-
+                overall.send_keys(Keys.ARROW_DOWN)
+    time.sleep(0.5)
     driver.implicitly_wait(5)
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
-# Find their matches
-match_history = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="TeamOverview_MatchHistory"]')
-matches = match_history.find_elements(By.CSS_SELECTOR, 'li')
-
-# Scrape each match
-data = [TEAM_NAME]
-for match in matches:
-    data.append([])
-    match.find_element(By.CSS_SELECTOR, 'img').click()
-
-    time.sleep(4)
-    driver.implicitly_wait(5)
-    
-    driver.switch_to.window(driver.window_handles[1])
-    banner = driver.find_element(By.CSS_SELECTOR,
-                                 'div[data-testid="NxEsportBanner__TextContainer"]')
-    is_home = banner.find_element(By.CSS_SELECTOR, 'span').text == TEAM_NAME
-
-    scoreboard = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="scoreboard-rows"]')
-    for position, series in enumerate(scoreboard.find_elements(By.XPATH, '*')):
-        data[-1].append({})
+def scrape_matches(s):
+    try:
+        driver.find_element(By.XPATH, '//div[text()="Show More"]').click()
+    except NoSuchElementException:
+        pass
+    match_history = driver.find_element(By.CSS_SELECTOR,
+                                        'div[data-testid="TeamOverview_MatchHistory"]')
+    matches = match_history.find_elements(By.CSS_SELECTOR, 'li')
+    for match in matches:
         try:
-            header, body = series.find_elements(By.XPATH, '*')
-            texts = [p.text for p in header.find_elements(By.CSS_SELECTOR, 'p') if p.text]
-            names = [t for t in texts if all(x.isalpha() or x.isspace() for x in t)]
-            data[-1][-1]['name'] = names[0 if is_home else 1]
-            data[-1][-1]['games'] = []
-            for game in body.find_elements(By.XPATH, '*/*'):
-                data[-1][-1]['games'].append({})
-                stage = game.find_element(By.XPATH, '//p[contains(text(), "Game")]'
-                                                    '/following-sibling::p').text
-                data[-1][-1]['games'][-1]['stage'] = stage
-                chars = [c.get_attribute('alt') for c in game.find_elements(By.CSS_SELECTOR, 'img')]
-                home_won = game.find_element(By.CSS_SELECTOR,
-                                            'div[data-cy="leftTriangle"]').is_displayed()
-                if is_home:
-                    data[-1][-1]['games'][-1]['char'] = chars[0]
-                    data[-1][-1]['games'][-1]['opp'] = chars[1]
-                    data[-1][-1]['games'][-1]['result'] = 'win' if home_won else 'loss'
-                else:
-                    data[-1][-1]['games'][-1]['char'] = chars[1]
-                    data[-1][-1]['games'][-1]['opp'] = chars[0]
-                    data[-1][-1]['games'][-1]['result'] = 'win' if not home_won else 'loss'
-        except ValueError:
-            header = series.find_elements(By.XPATH, '*')[0]
-            texts = [p.text for p in header.find_elements(By.CSS_SELECTOR, 'p') if p.text]
-            names = [t for t in texts if all(x.isalpha() or x.isspace() for x in t)]
-            data[-1][-1]['name'] = names[0 if is_home else 1]
-            data[-1][-1]['games'] = []
+            match.find_element(By.CSS_SELECTOR, 'img').click()
+            data.append([])
+            time.sleep(4)
+            driver.implicitly_wait(5)
+
+            driver.switch_to.window(driver.window_handles[1])
+            banner = driver.find_element(By.CSS_SELECTOR,
+                                        'div[data-testid="NxEsportBanner__TextContainer"]')
+            is_home = banner.find_element(By.CSS_SELECTOR, 'span').text == TEAM_NAME
+
+            scoreboard = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="scoreboard-rows"]')
+            for series in scoreboard.find_elements(By.XPATH, '*'):
+                data[-1].append({})
+                try:
+                    header, body = series.find_elements(By.XPATH, '*')
+                    texts = [p.text for p in header.find_elements(By.CSS_SELECTOR, 'p') if p.text]
+                    names = [t for t in texts if all(x.isalpha() or x in " -'"  for x in t)]
+                    data[-1][-1]['name'] = names[0 if is_home else 1]
+                    data[-1][-1]['games'] = []
+                    for game in body.find_elements(By.XPATH, '*/*'):
+                        data[-1][-1]['games'].append({})
+                        data[-1][-1]['games'][-1]['season'] = s
+                        stage = game.find_element(By.XPATH, '//p[contains(text(), "Game")]'
+                                                            '/following-sibling::p').text
+                        data[-1][-1]['games'][-1]['stage'] = stage
+                        chars = [c.get_attribute('alt') 
+                                for c in game.find_elements(By.CSS_SELECTOR, 'img')]
+                        home_won = game.find_element(By.CSS_SELECTOR,
+                                                    'div[data-cy="leftTriangle"]').is_displayed()
+                        if is_home:
+                            data[-1][-1]['games'][-1]['char'] = chars[0]
+                            data[-1][-1]['games'][-1]['opp'] = chars[1]
+                            data[-1][-1]['games'][-1]['result'] = 'win' if home_won else 'loss'
+                        else:
+                            data[-1][-1]['games'][-1]['char'] = chars[1]
+                            data[-1][-1]['games'][-1]['opp'] = chars[0]
+                            data[-1][-1]['games'][-1]['result'] = 'win' if not home_won else 'loss'
+                        print(data[-1][-1]['name'], data[-1][-1]['games'][-1])
+                except ValueError:
+                    header = series.find_elements(By.XPATH, '*')[0]
+                    texts = [p.text for p in header.find_elements(By.CSS_SELECTOR, 'p') if p.text]
+                    names = [t for t in texts if all(x.isalpha() or x in " -'" for x in t)]
+                    data[-1][-1]['name'] = names[0 if is_home else 1]
+                    data[-1][-1]['games'] = []
+            driver.switch_to.window(driver.window_handles[0])
+        except NoSuchElementException:
+            pass
+
+    windows = len(driver.window_handles)
+    for _ in range(windows-1):
+        driver.switch_to.window(driver.window_handles[-1])
+        driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
-windows = len(driver.window_handles)
-for i in range(windows):
-    driver.switch_to.window(driver.window_handles[0])
-    driver.close()
+data = [TEAM_NAME]
+driver.find_element(By.XPATH, '//p[text()="Spring 2025"]').click()
+seasons = [s.find_element(By.CSS_SELECTOR, 'p').text for s in driver.find_elements(By.XPATH,
+                        '//li[contains(@data-cy, "Spring") or contains(@data-cy, "Fall")]')][::-1]
+action_chains.key_down(Keys.ESCAPE).key_up(Keys.ESCAPE).perform()
+scrape_matches('Spring 2025')
+for i in range(1, len(seasons)):
+    if int(seasons[i].split(' ')[1]) > 2022:
+        driver.find_element(By.XPATH, f'//p[text()="{seasons[i-1]}"]').click()
+        driver.find_element(By.XPATH, f'//p[text()="{seasons[i]}"]').click()
+        driver.implicitly_wait(5)
+        scrape_matches(seasons[i])
+        try:
+            driver.find_element(By.XPATH, '//p[text()="Playoffs - Semi Finals / Finals"]').click()
+            driver.find_element(By.XPATH, '//p[text()="Playoffs"]').click()
+            driver.implicitly_wait(5)
+            scrape_matches(seasons[i])
+        except NoSuchElementException:
+            pass
+        driver.find_element(By.XPATH, '//p[text()="Playoffs"]').click()
+        driver.find_element(By.XPATH, '//p[text()="Regular Season"]').click()
+        driver.implicitly_wait(5)
+        scrape_matches(seasons[i])
+driver.close()
 
 with open('data.json', 'w', encoding='UTF-8') as f:
     f.write(json.dumps(data, indent=4))
